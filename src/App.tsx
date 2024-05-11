fa7a92bb43acca7370a0197a23f8a4ff99c845e5
Louai Zahran <louai0nasr@gmail.com>
2024-05-11 04:39:17 +0300

    Merging NFRD-32-create-simple-web-app

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+import {NFRDService} from "./NFRDService";
+
+function App() {
+    const [inputData, setInputData] = useState('');
+    const [analysisResult, setAnalysisResult] = useState('');
+
+    return (
+        <div className="app-container">
+            <h1>NFRD</h1>
+            <div className="input-container">
+                <label htmlFor="inputData">Enter Text:</label>
+                <textarea
+                    id="inputData"
+                    value={inputData}
+                    onChange={(e) => setInputData(e.target.value)}
+                    placeholder="Type your text here..."
+                />
+            </div>
+            <button onClick={async () => {await  NFRDService(inputData, setAnalysisResult);}}>Analyze</button>
+            <div className="result-container">
+                <h2>Analysis Result:</h2>
+                <p>{analysisResult}</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/NFRDService.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const NFRDService = async (inputData: string, setAnalysisResult: React.Dispatch<React.SetStateAction<string>>) => {
+    try {
+        const response = await fetch('http://localhost:8081/analyze', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: inputData }),
+        });
+
+        const result = await response.json();
+
+        if (result && 'isRumor' in result && 'label_0_probability' in result && 'label_1_probability' in result) {
+            const analysisResult = result.isRumor
+                ? `This might be a rumor. Probability: ${result.label_1_probability}`
+                : `No indication of a rumor. Probability: ${result.label_0_probability}`;
+
+
+            setAnalysisResult(analysisResult);
+        } else {
+            console.error('Invalid response format:', result);
+            setAnalysisResult('Error: Invalid response format.');
+        }
+    } catch (error) {
+        console.error('Error calling API:', error);
+        setAnalysisResult('Error: Unable to connect to the API.');
+    }
+};
