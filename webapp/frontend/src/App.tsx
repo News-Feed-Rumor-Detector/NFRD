cf622cfb1eb88e572810a22611639a4cbd629ff7
bazina <youssefbazina7@gmail.com>
2024-06-13 16:00:46 +0300

    New UI for the rumor and real news

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import React, {useState} from 'react';
 import './App.css';
-import {NFRDService} from "./NFRDService";
+import {NFDRDResult, NFRDService} from "./NFRDService";
 
 function App() {
     const [inputData, setInputData] = useState('');
-    const [analysisResult, setAnalysisResult] = useState('');
+    const [analysisResult, setAnalysisResult] = useState<NFDRDResult>(
+        {
+            isRumor: false,
+            confidence: 0,
+            prediction: -1
+        }
+    );
 
     return (
         <div className="app-container">
             <h1>NFRD</h1>
             <div className="input-container">
-                <label htmlFor="inputData">Enter Text:</label>
+                <label htmlFor="inputData">Enter Text</label>
                 <textarea
                     id="inputData"
                     value={inputData}
@@ -18,11 +24,20 @@ function App() {
                     placeholder="Type your text here..."
                 />
             </div>
-            <button onClick={async () => {await  NFRDService(inputData, setAnalysisResult);}}>Analyze</button>
-            <div className="result-container">
-                <h2>Analysis Result:</h2>
-                <p>{analysisResult}</p>
-            </div>
+            <button className="analyze-button" onClick={async () => {
+                await NFRDService(inputData, setAnalysisResult);
+            }}>
+                Analyze
+            </button>
+            {analysisResult.prediction !== -1 && (
+                <div className="result-container">
+                    <h2>Analysis Result</h2>
+                    <div className={analysisResult.isRumor ? 'result-true' : 'result-false'}>
+                        <p>{analysisResult.isRumor ? 'Rumor' : 'Real News'}</p>
+                        <p>{analysisResult.confidence.toFixed(1)}%</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
