18201598b0333fc91f3212fc86fa71d8cb41ea59
bazina <youssefbazina7@gmail.com>
2024-06-13 17:01:31 +0300

    - Add interface for the result - Return promise of type NFRD Result instead of a string

--- webapp/frontend/src/NFRDService.tsx
+++ webapp/frontend/src/NFRDService.tsx
@@ -1,19 +1,30 @@
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
+interface ModelResponse {
+    confidence: number;
+    prediction: number;
+}
 
-export const NFRDService = async (inputData: string, setAnalysisResult: React.Dispatch<React.SetStateAction<string>>) => {
+export interface NFRDResult {
+    isRumor: boolean;
+    confidence: number;
+    prediction: number;
+}
+
+export const NFRDService = async (inputData: string, setAnalysisResult: React.Dispatch<React.SetStateAction<NFRDResult>>) => {
     const apiUrl = 'http://172.166.85.39'; // Azure model endpoint
     const headers = {
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
     };
-  
+
     try {
-      const response = await axios.get(apiUrl, {
-        headers,
-        data: {
-          input: inputData
-        }
-      });
+        const response = await axios.get(apiUrl, {
+            headers,
+            data: {
+                input: inputData
+            }
+        });
         const responseData = response.data;
 
         // Check if the response data contains confidence and prediction
@@ -24,30 +35,27 @@ export const NFRDService = async (inputData: string, setAnalysisResult: React.Di
         // Interpret prediction
         const result = interpretPrediction(responseData);
 
-        if (result.isRumor !== undefined && result.confidence !== undefined) {
-            const analysisResult = result.isRumor
-                ? `This might be a rumor. Confidence: ${result.confidence}`
-                : `No indication of a rumor. Confidence: ${result.confidence}`;
+        // Dummy data uncomment for testing and comment out the above lines
+        // let rnd = Math.random();
+        // const result: NFDRDResult = {
+        //     confidence: parseFloat(rnd.toFixed(3)) * 100,
+        //     isRumor: rnd > 0.5,
+        //     prediction: rnd > 0.5 ? 1 : 0,
+        // };
 
-            setAnalysisResult(analysisResult);
-        } else {
+        if (!(result.isRumor !== undefined && result.confidence !== undefined)) {
+            result.prediction = -2;
             console.error('Invalid response format:', result);
-            setAnalysisResult('Error: Invalid response format.');
         }
+        setAnalysisResult(result);
     } catch (error) {
         console.error('Error calling Azure API:', error);
-        setAnalysisResult('Error: Unable to connect to the Azure API.');
+        setAnalysisResult({prediction: 404, isRumor:false, confidence:0});
     }
 };
 
-interface  ModelResponse{
-    confidence: number;
-    prediction: number;
-}
-
-
 // Function to interpret prediction
-const interpretPrediction = (response: ModelResponse) => {
+const interpretPrediction = (response: ModelResponse): NFRDResult => {
     // Check if response is valid
     if (!response || !response.hasOwnProperty('confidence') || !response.hasOwnProperty('prediction')) {
         throw new Error('Invalid response format: Missing confidence or prediction.');
@@ -55,10 +63,11 @@ const interpretPrediction = (response: ModelResponse) => {
 
     // Check if the confidence is higher than the threshold
     const isRumor = response.prediction === 1; // 1 indicates a rumor
+    const confidence: number = parseFloat(response.confidence.toFixed(3)) * 100;
 
     return {
         isRumor,
-        confidence: response.confidence,
+        confidence: confidence,
         prediction: response.prediction,
     };
 };
