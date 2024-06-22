b6316803e6f75ddb2b290f03fee007081472c28f
Abdelrahman Elsayed <77246893+Mento79@users.noreply.github.com>
2024-06-22 23:53:49 +0300

    Update  UI (#18)
    * New UI for the rumor and real news
    
    * Modify CSS
    
    * - Add interface for the result
    - Return promise of type NFRD Result instead of a string
    
    * - Modify UI and use NFRDResult interface
    
    * Fix typo
    
    * Adding Rumor Stacked Bar
    
    * fix the confidence dependence
    
    ---------
    
    Co-authored-by: bazina <youssefbazina7@gmail.com>

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -1,16 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
-import {NFRDService} from "./NFRDService";
+import {NFRDResult, NFRDService} from "./NFRDService";
+import StackedBarChart from './StackedBarChart';
 
 function App() {
     const [inputData, setInputData] = useState('');
-    const [analysisResult, setAnalysisResult] = useState('');
+    const [analysisResult, setAnalysisResult] = useState<NFRDResult>(
+        {
+            isRumor: false,
+            confidence: 0,
+            prediction: -1
+        }
+    );
+    
+    const predPrecentage = Math.round(analysisResult.confidence * 10) / 10 
 
+    const rumorPercentage = analysisResult.isRumor 
+    ?predPrecentage
+    :Math.round((100 - predPrecentage) * 10) / 10;
+
+    const realPercentage = analysisResult.isRumor 
+    ?Math.round((100 - predPrecentage) * 10) / 10
+    :predPrecentage;
+    
     return (
         <div className="app-container">
             <h1>NFRD</h1>
             <div className="input-container">
-                <label htmlFor="inputData">Enter Text:</label>
+                <label htmlFor="inputData">Enter Text</label>
                 <textarea
                     id="inputData"
                     value={inputData}
@@ -18,13 +35,35 @@ function App() {
                     placeholder="Type your text here..."
                 />
             </div>
-            <button onClick={async () => {await  NFRDService(inputData, setAnalysisResult);}}>Analyze</button>
+            <button className="analyze-button" onClick={async () => {
+                await NFRDService(inputData, setAnalysisResult);
+            }}>
+                Analyze
+            </button>
+
             <div className="result-container">
-                <h2>Analysis Result:</h2>
-                <p>{analysisResult}</p>
+                <h2>Analysis Result</h2>
+                {(analysisResult.prediction === 0 || analysisResult.prediction === 1) && (
+                    <div className={analysisResult.isRumor ? 'result-true' : 'result-false'}>
+                        <p>{analysisResult.isRumor ? 'Rumor' : 'Real'}</p>
+                        {/* <p>Confidence = {analysisResult.confidence.toFixed(1)}%</p> */}
+                    </div>
+                )}
+                {analysisResult.prediction === 404 && (
+                    <div className={'result-true'}>
+                        <p>Error: Unable to connect to the Azure API</p>
+                    </div>
+                )}
+                
             </div>
+            {(analysisResult.prediction === 0 || analysisResult.prediction === 1) && (
+                <StackedBarChart part1Percentage={realPercentage} part2Percentage={rumorPercentage} />
+        )}
+
+
         </div>
     );
 }
+
 
 export default App;
